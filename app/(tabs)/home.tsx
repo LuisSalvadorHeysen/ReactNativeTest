5dba257337cdfee54b75a6363df97cca3fb79158
LuisHeysen <lsalvador.ht@gmail.com>
2024-07-12 19:27:11 -0500

    modified:   app/(tabs)/home.tsx 	modified:   components/Trending.tsx 	modified:   lib/appwrite.js 	modified:   package-lock.json 	modified:   package.json

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,5 +1,5 @@
-import { Image, View, Text, RefreshControl, StatusBar } from 'react-native'
-import React, { useState } from 'react'
+import { Image, View, Text, RefreshControl, StatusBar, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { FlatList } from 'react-native'
 
@@ -7,26 +7,30 @@ import { images } from "../../constants"
 import SearchInput from '@/components/SearchInput';
 import Trending from '@/components/Trending'
 import EmptyState from '@/components/EmptyState'
+import VideoCard from '@/components/VideoCard'
+import { getAllPosts, getLatestPosts } from '@/lib/appwrite'
+import useAppwrite from '@/lib/useAppwrite'
 
 const Home = () => {
-  const [data, setData] = useState(false)
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const [refreshing, setRefreshing] = useState(false)
 
   const onRefresh = async () => {
     setRefreshing(true);
-    // re call videos to see if any new videos appear
+    await refetch();
     setRefreshing(false);
   }
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         // data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <VideoCard video={item} />
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
@@ -56,7 +60,7 @@ const Home = () => {
                 Latest Videos
               </Text>
 
-              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+              <Trending posts={latestPosts} />
             </View>
           </View>
         )}
